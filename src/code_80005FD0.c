63d0debda7b7f407995c765951e4d6f848774437
coco875 <pereira.jannin@gmail.com>
2024-12-26 03:57:10 +0100

    Update code_80005FD0.c

--- src/code_80005FD0.c
+++ src/code_80005FD0.c
@@ -1664,16 +1664,16 @@ void func_80009B60(s32 playerId) {
         if (!(player->unk_0CA & 2) && !(player->unk_0CA & 8)) {
             D_80163448 = gPathIndexByPlayerId[playerId];
             func_80008DC0(D_80163448);
-            //if (GetCourse() == GetKalimariDesert()) {
-                CourseManager_VehiclesCollision(playerId, player);
-                //func_80012DC0(playerId, player);
-                if (playerId == 0) {
-                    CourseManager_CrossingTrigger();
-                    //func_80013054();
-                }
+            // if (GetCourse() == GetKalimariDesert()) {
+            CourseManager_VehiclesCollision(playerId, player);
+            // func_80012DC0(playerId, player);
+            if (playerId == 0) {
+                CourseManager_CrossingTrigger();
+                // func_80013054();
+            }
             //}
             if (GetCourse() == GetDkJungle()) {
-                //func_80013854(player);
+                // func_80013854(player);
             } else if (GetCourse() == GetToadsTurnpike()) {
                 func_800148C4(playerId, player);
                 func_80014A18(playerId, player);
@@ -1760,7 +1760,7 @@ void func_80009B60(s32 playerId) {
                         D_80163210[playerId] = CourseManager_GetProps()->D_0D009568[gCCSelection];
                     }
                     CourseManager_AICrossingBehaviour(playerId);
-                    //check_ai_crossing_distance(playerId);
+                    // check_ai_crossing_distance(playerId);
                     func_8000D3B8(playerId);
                     func_8000D438(playerId, D_801630E0);
                     temp_f0 = D_80162FA0[0] - player->pos[0];
@@ -1974,7 +1974,7 @@ void func_80009B60(s32 playerId) {
                 player->effects &= ~0x00200000;
                 D_80163210[playerId] = D_8016320C;
                 CourseManager_AICrossingBehaviour(playerId);
-                //check_ai_crossing_distance(playerId);
+                // check_ai_crossing_distance(playerId);
                 func_80008424(playerId, D_8016320C, player);
             }
         }
@@ -2907,7 +2907,7 @@ void set_bomb_kart_spawn_positions(void) {
     BombKartSpawn* bombKartSpawn;
 
     for (var_s3 = 0; var_s3 < NUM_BOMB_KARTS_VERSUS; var_s3++) {
-        //bombKartSpawn = &gBombKartSpawns[gCurrentCourseId][var_s3];
+        // bombKartSpawn = &gBombKartSpawns[gCurrentCourseId][var_s3];
         if (GetCourse() == GetYoshiValley()) {
             startingXPos = bombKartSpawn->startingXPos;
             startingZPos = bombKartSpawn->startingZPos;
@@ -3449,7 +3449,7 @@ void func_8000F2DC(void) {
 
     CourseManager_SpawnVehicles();
 
-    //set_bomb_kart_spawn_positions();
+    // set_bomb_kart_spawn_positions();
     func_8000EEDC();
 }
 
@@ -4148,10 +4148,10 @@ void kart_ai_behaviour_start(s32 playerId, Player* player) {
     s32 behaviourType;
     UNUSED s32 test;
 
-    KartAIBehaviour *beh = (KartAIBehaviour*)LOAD_ASSET(CourseManager_GetProps()->AIBehaviour);
+    KartAIBehaviour* beh = (KartAIBehaviour*) LOAD_ASSET(CourseManager_GetProps()->AIBehaviour);
 
-    sCurrentKartAIBehaviour = 
-        &((KartAIBehaviour*)LOAD_ASSET(CourseManager_GetProps()->AIBehaviour))[gCurrentKartAIBehaviourId[playerId]];
+    sCurrentKartAIBehaviour =
+        &((KartAIBehaviour*) LOAD_ASSET(CourseManager_GetProps()->AIBehaviour))[gCurrentKartAIBehaviourId[playerId]];
 
     playerWaypoint = gNearestWaypointByPlayerId[playerId];
 
@@ -4326,7 +4326,8 @@ void generate_ferry_waypoints(void) {
     D_80162EB2 = -40;
 }
 
-void spawn_vehicle_on_road(Vec3f position, Vec3s rotation, Vec3f velocity, s32 waypointIndex, s32 someMultiplierTheSequel, f32 speed) {
+void spawn_vehicle_on_road(Vec3f position, Vec3s rotation, Vec3f velocity, s32 waypointIndex,
+                           s32 someMultiplierTheSequel, f32 speed) {
     f32 origXPos;
     UNUSED f32 pad;
     f32 origZPos;
@@ -4334,14 +4335,12 @@ void spawn_vehicle_on_road(Vec3f position, Vec3s rotation, Vec3f velocity, s32 w
     origXPos = position[0];
     origZPos = position[2];
     if (D_8016347A == 0) {
-        func_8000D6D0(position, (s16*) &waypointIndex, speed,
-                      someMultiplierTheSequel, 0, 3);
+        func_8000D6D0(position, (s16*) &waypointIndex, speed, someMultiplierTheSequel, 0, 3);
         rotation[0] = 0;
         rotation[1] = -0x8000;
         rotation[2] = 0;
     } else {
-        func_8000D940(position, (s16*) &waypointIndex, speed,
-                      someMultiplierTheSequel, 0);
+        func_8000D940(position, (s16*) &waypointIndex, speed, someMultiplierTheSequel, 0);
         rotation[0] = 0;
         rotation[1] = 0;
         rotation[2] = 0;
@@ -4424,9 +4423,9 @@ void init_vehicles_trains(size_t i, size_t numCarriages, f32 speed) {
 
     gTrainList[i].numCarriages = numCarriages;
 
-    //for (i = 0; i < NUM_TRAINS; i++) {
-    // outputs 160 or 392 depending on the train.
-    // Wraps the value around to always output a valid waypoint.
+    // for (i = 0; i < NUM_TRAINS; i++) {
+    //  outputs 160 or 392 depending on the train.
+    //  Wraps the value around to always output a valid waypoint.
     waypointOffset = (((i * gVehicle2DWaypointLength) / 2) + 160) % gVehicle2DWaypointLength;
 
     // 120.0f is about the maximum usable value
@@ -6990,11 +6989,23 @@ void kart_ai_decisions_branch_item(UNUSED s32 arg0, s16* arg1, s32 arg2) {
         case ITEM_MUSHROOM:
             value = 0x1D;
             break;
+        case ITEM_GREEN_SHELL:
+            value = 4;
+            break;
+        case ITEM_RED_SHELL:
+            value = 7;
+            break;
+        case ITEM_BANANA_BUNCH:
+            value = 10;
+            break;
         case ITEM_DOUBLE_MUSHROOM:
+            value = 30;
             break;
         case ITEM_TRIPLE_MUSHROOM:
+            value = 31;
             break;
         case ITEM_SUPER_MUSHROOM:
+            value = 32;
             break;
     }
     if (value >= 0) {
@@ -7035,10 +7046,9 @@ void kart_ai_use_item_strategy(s32 playerId) {
         switch (temp_s0->unk_00) {
             case 0:
                 temp_s0->actorIndex = -1;
-                if ((((playerId * 0x14) + 0x64) < D_80164450[playerId]) && (temp_s0->unk_04 >= 0x259) &&
-                    (temp_s0->unk_06 < 3) && (gLapCountByPlayerId[playerId] < 3)) {
+                if ((temp_s0->unk_04 % 120) == 0) {
                     kart_ai_decisions_branch_item(playerId, &temp_s0->unk_00,
-                                                  kart_ai_gen_random_item((s16) gLapCountByPlayerId[playerId],
+                                                  gen_random_item_human((s16) gLapCountByPlayerId[playerId],
                                                                           gGPCurrentRaceRankByPlayerId[playerId]));
                 } else {
                     func_8001ABE0(playerId, temp_s0);
@@ -7047,7 +7057,7 @@ void kart_ai_use_item_strategy(s32 playerId) {
 
             case 1:
                 if ((gLapCountByPlayerId[playerId] > 0) &&
-                    (gGPCurrentRaceRankByPlayerId[D_80163478] > gGPCurrentRaceRankByPlayerId[playerId]) &&
+                    (gGPCurrentRaceRankByPlayerId[D_80163478] < gGPCurrentRaceRankByPlayerId[playerId]) &&
                     (gGPCurrentRaceRankByPlayerId[D_80163478] == 0)) {
                     switch (player->characterId) {
                         case 4:
@@ -7184,7 +7194,7 @@ void kart_ai_use_item_strategy(s32 playerId) {
                 break;
 
             case 4:
-                if (((s32) gNumActors) < 0x50) {
+                if (((s32) gNumActors) < 0x50 || 1) {
                     temp_s0->actorIndex = use_green_shell_item(player);
                     if ((temp_s0->actorIndex >= 0) && (temp_s0->actorIndex < 0x64)) {
                         temp_s0->unk_00 = 5;
@@ -7225,7 +7235,7 @@ void kart_ai_use_item_strategy(s32 playerId) {
                 break;
 
             case 7:
-                if (((s32) gNumActors) < 0x50) {
+                if (((s32) gNumActors) < 0x50 || 1) {
                     temp_s0->actorIndex = use_red_shell_item(player);
                     if ((temp_s0->actorIndex >= 0) && (temp_s0->actorIndex < 0x64)) {
                         temp_s0->unk_00 = 8;
@@ -7266,7 +7276,7 @@ void kart_ai_use_item_strategy(s32 playerId) {
                 break;
 
             case 10:
-                if (((s32) gNumActors) < 0x50) {
+                if (((s32) gNumActors) < 0x50 || 1) {
                     temp_s0->actorIndex = use_banana_bunch_item(player);
                     if ((temp_s0->actorIndex >= 0) && (temp_s0->actorIndex < 0x64)) {
                         temp_s0->unk_00 = 0x000B;
