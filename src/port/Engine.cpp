4ec1045c3d18cd7dfeb1f93e011aa7111546e50c
coco875 <pereira.jannin@gmail.com>
2025-05-22 15:09:55 +0200

    update torch and lus

--- src/port/Engine.cpp
+++ src/port/Engine.cpp
@@ -510,10 +510,11 @@ ImFont* GameEngine::CreateFontWithSize(float size, std::string fontPath) {
         initData->Type = static_cast<uint32_t>(RESOURCE_TYPE_FONT);
         initData->ResourceVersion = 0;
         initData->Path = fontPath;
-        auto file = Ship::Context::GetInstance()->GetResourceManager()->LoadFileProcess(fontPath);
         std::shared_ptr<Ship::Font> fontData = std::static_pointer_cast<Ship::Font>(
-            Ship::Context::GetInstance()->GetResourceManager()->GetResourceLoader()->LoadResource(fontPath, file, initData));
-        font = mImGuiIo->Fonts->AddFontFromMemoryTTF(fontData->Data, fontData->DataSize, size);
+            Ship::Context::GetInstance()->GetResourceManager()->LoadResource(fontPath, false, initData));
+        char* fontDataPtr = (char*)malloc(fontData->DataSize);
+        memcpy(fontDataPtr, fontData->Data, fontData->DataSize);
+        font = mImGuiIo->Fonts->AddFontFromMemoryTTF(fontDataPtr, fontData->DataSize, size);
     }
     // FontAwesome fonts need to have their sizes reduced by 2.0f/3.0f in order to align correctly
     float iconFontSize = size * 2.0f / 3.0f;
