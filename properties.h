f83f0ac40a694dd4e67fca1daf847a9b1b28e1d2
sitton76 <58642183+sitton76@users.noreply.github.com>
2025-06-20 12:01:54 -0500

    Corrections.

--- properties.h
+++ properties.h
@@ -5,10 +5,10 @@
 #define VER_PRODUCTVERSION       0, 1, 0, 0
 #define VER_PRODUCTVERSION_str   "0.1.0\0"
 
-#define VER_COMPANYNAME_STR      "Lywx & YoshiCrystal\0"
-#define VER_PRODUCTNAME_STR      "Starship\0"
+#define VER_COMPANYNAME_STR      "Spaghettikart Team - Harbourmasters\0"
+#define VER_PRODUCTNAME_STR      "Spaghettikart\0"
 
-#define VER_INTERNALNAME_STR     "Starship\0"
-#define VER_ORIGINALFILENAME_STR "Starship.exe\0"
+#define VER_INTERNALNAME_STR     "Spaghettikart\0"
+#define VER_ORIGINALFILENAME_STR "Spaghettify.exe\0"
 
-#define VER_FILEDESCRIPTION_STR  "Starship - Barnard Alfa\0"
+#define VER_FILEDESCRIPTION_STR  "Spaghettikart -  Alfredo Alfa\0"
