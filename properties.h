c1aae799e7aa0d45189744909b067827f983f0f8
sitton76 <58642183+sitton76@users.noreply.github.com>
2025-06-20 11:29:19 -0500

    Copied stuff over for Window icon from Starship.

---
+++ properties.h
@@ -0,0 +1,14 @@
+
+#define VER_FILEVERSION          0, 1, 0, 0
+#define VER_FILEVERSION_STR      "0.1.0\0"
+
+#define VER_PRODUCTVERSION       0, 1, 0, 0
+#define VER_PRODUCTVERSION_str   "0.1.0\0"
+
+#define VER_COMPANYNAME_STR      "Lywx & YoshiCrystal\0"
+#define VER_PRODUCTNAME_STR      "Starship\0"
+
+#define VER_INTERNALNAME_STR     "Starship\0"
+#define VER_ORIGINALFILENAME_STR "Starship.exe\0"
+
+#define VER_FILEDESCRIPTION_STR  "Starship - Barnard Alfa\0"
